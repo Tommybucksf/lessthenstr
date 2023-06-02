50728811052e6f7a8fa4583bd047d7d0fee2fd0c
Tommybucksf <110362455+Tommybucksf@users.noreply.github.com>
2023-06-02 15:08:16 +0300

    Add files via upload
    bed try

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//___Програма яка рахує переиметр___
+//  main.cpp
+//  HW
+//
+//  Created by B on 11.05.2023.
+//
+
+#include <iostream>
+
+using namespace std;
+int main()
+
+{
+    double a, b;
+    cin >> a >> b;
+    double p = 2*(a+b);
+
+    
+    cout << "p= " << p;
+    return 0;
+}
